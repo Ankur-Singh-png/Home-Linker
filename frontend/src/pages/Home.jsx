d249dbd19219a4ec7327f966c2e676262e035fc0
Yash-Agrawal-644 <90204382+Yash-Agrawal-644@users.noreply.github.com>
2025-07-24 19:56:45 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import About from '../components/About'
 import Properties from '../components/Properties'
 import Blogs from '../components/Blogs'
+import bannerImg from '../assets/banner.png'
 
 const Home = () => {
   return (
@@ -11,7 +12,11 @@ const Home = () => {
       <About />
       <Properties/>
       <Blogs />
-    </main>
+  
+     <div className='max-padd-container py-16 overflow-x-hidden'>
+        <img src={bannerImg}/>
+     </div>
+   </main>
   )
 }
 
