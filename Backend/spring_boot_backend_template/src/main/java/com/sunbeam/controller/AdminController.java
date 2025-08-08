6887d179afcca6cf7e82ac999ca3edbb028cd63a
Ankur Singh <93007035+Ankur-Singh-png@users.noreply.github.com>
2025-08-08 11:33:14 +0530

    Updated AdminController.java

--- Backend/spring_boot_backend_template/src/main/java/com/sunbeam/controller/AdminController.java
+++ Backend/spring_boot_backend_template/src/main/java/com/sunbeam/controller/AdminController.java
@@ -1,16 +1,35 @@
 package com.sunbeam.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sunbeam.services.BookingService;
+import com.sunbeam.services.UserService;
+import com.sunbeam.services.WishListService;
+
+import java.util.*;
+import com.sunbeam.dto.*;
+import com.sunbeam.entities.Booking;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/admin")
+@CrossOrigin
+@AllArgsConstructor
 public class AdminController {
+	private UserService userService;
+	private BookingService bookingService;
+	private WishListService wishListService;
 	
 	@GetMapping("/dashboard")
-	public String getDashBoard() {
-		return "dashboard fetched";
+	public ResponseEntity<?> getDashBoard() {
+		System.out.println("in admin");
+		List<UserDTO> users = userService.getAllUsers();
+		return ResponseEntity.ok(users);
 	}
 
 }
